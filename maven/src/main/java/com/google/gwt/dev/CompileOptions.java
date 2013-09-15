bf4ceebb374a2fc279c5622695469fba37111305
James.Nelson <James@wetheinter.net>
2013-09-15 05:02:57 -0600

    Delete deprecated code

--- maven/src/main/java/com/google/gwt/dev/CompileOptions.java
+++
@@ -1,7 +0,0 @@
-package com.google.gwt.dev;
-
-public class CompileOptions extends PrecompileTaskOptionsImpl{
-
-  private static final long serialVersionUID = -2906830173232136809L;
-
-}
--- maven/src/main/java/com/google/gwt/dev/GeneratorOptionsProcessor.java
+++
@@ -1,9 +0,0 @@
-package com.google.gwt.dev;
-
-public class GeneratorOptionsProcessor extends PrecompileTaskArgProcessor{
-
-  public GeneratorOptionsProcessor(PrecompileTaskOptions options) {
-    super(options);
-  }
-
-}
