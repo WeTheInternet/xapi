d30c6e3afcc4f9774ba2454f5e2621d0506fa753
James.Nelson <James@wetheinter.net>
2014-02-23 17:53:10 -0500

    [maven-release-plugin] prepare release xapi-0.4

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-mvn release:prepare -Darguments="-Dsource.forceCreation=true -Dsource.includePom=true -Dxapi.skip.test=true -Dxapi.release=true"
-mvn release:perform -Darguments="-Dsource.forceCreation=true -Dsource.includePom=true -Dxapi.skip.test=true -Dxapi.release=true" -Dgoals=deploy
+mvn release:prepare -Darguments="-Dsource.forceCreation=true -Dsource.includePom=true -Dxapi.skip.test=true -Dxapi.release=true" && mvn release:perform -Darguments="-Dsource.forceCreation=true -Dsource.includePom=true -Dxapi.skip.test=true -Dxapi.release=true" -Dgoals=deploy
 
