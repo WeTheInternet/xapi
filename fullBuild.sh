12969a428d2ab2a8edc848d99760ce4867944342
James.Nelson <James@wetheinter.net>
2014-02-28 11:25:17 -0500

    Update quick build scripts

--- fullBuild.sh
+++ fullBuild.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-mvn clean install -f settings/pom.xml -T 2.5C -Dxapi.log.level=INFO -Dxapi.prod=true -Dxapi.debug=false -Dxapi.release=true -Dxapi.skip.test=false $@
+mvn clean install -T 2.5C -Dxapi.log.level=INFO -Dxapi.prod=true -Dxapi.debug=false -Dxapi.release=true -Dxapi.skip.test=false $@
