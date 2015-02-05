f72e9f931c1b3e04e643bce2dff261d2a1ebdb0d
James.Nelson <James@wetheinter.net>
2015-02-05 02:20:57 -0500

    Fix broken lexer test

--- gwt/elemental/src/test/java/xapi/elemental/impl/MarkupLexerTest.java
+++ gwt/elemental/src/test/java/xapi/elemental/impl/MarkupLexerTest.java
@@ -7,41 +7,41 @@ public class MarkupLexerTest {
 
   private static final String HELLO_WORLD = "Hello World";
   private static final String HELLO_WORLD_LINK = "<a "
-      + "href=\"/"+HELLO_WORLD+"\" >"
+      + "href=\"/"+HELLO_WORLD.replace(' ', '-')+"\" >"
       + HELLO_WORLD+ "</a>";
 
   @Test
   public void testLinkFormat_OnlyLink() {
-    LexerForMarkup lex = new LexerForMarkup();
+    final LexerForMarkup lex = new LexerForMarkup();
     lex.lex("#["+HELLO_WORLD+ "]");
     Assert.assertEquals(HELLO_WORLD_LINK, lex.toSource());
   }
 
   @Test
-  public void testLinkFormat_WhitespaceBeforeLink() {
-    LexerForMarkup lex = new LexerForMarkup();
-    lex.lex("  #["+HELLO_WORLD+ "]");
-    Assert.assertEquals("  "+HELLO_WORLD_LINK, lex.toSource());
-  }
-
-  @Test
-  public void testLinkFormat_WordBeforeLink() {
-    LexerForMarkup lex = new LexerForMarkup();
-    lex.lex(" Word #["+HELLO_WORLD+ "]");
-    Assert.assertEquals(" Word "+HELLO_WORLD_LINK, lex.toSource());
-  }
-
-  @Test
   public void testLinkFormat_WhitespaceAfterLink() {
-    LexerForMarkup lex = new LexerForMarkup();
+    final LexerForMarkup lex = new LexerForMarkup();
     lex.lex("  #["+HELLO_WORLD+ "]  ");
     Assert.assertEquals("  "+HELLO_WORLD_LINK+"  ", lex.toSource());
   }
 
   @Test
+  public void testLinkFormat_WhitespaceBeforeLink() {
+    final LexerForMarkup lex = new LexerForMarkup();
+    lex.lex("  #["+HELLO_WORLD+ "]");
+    Assert.assertEquals("  "+HELLO_WORLD_LINK, lex.toSource());
+  }
+
+  @Test
   public void testLinkFormat_WordAfterLink() {
-    LexerForMarkup lex = new LexerForMarkup();
+    final LexerForMarkup lex = new LexerForMarkup();
     lex.lex(" Word #["+HELLO_WORLD+ "]After");
     Assert.assertEquals(" Word "+HELLO_WORLD_LINK+"After", lex.toSource());
+  }
+
+  @Test
+  public void testLinkFormat_WordBeforeLink() {
+    final LexerForMarkup lex = new LexerForMarkup();
+    lex.lex(" Word #["+HELLO_WORLD+ "]");
+    Assert.assertEquals(" Word "+HELLO_WORLD_LINK, lex.toSource());
   }
 }
